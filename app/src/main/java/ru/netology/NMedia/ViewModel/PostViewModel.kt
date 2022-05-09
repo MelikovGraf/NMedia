10960fb8a33d58bdec44ae4128d0ba56fdccc06c
Melikovgraf <Melikovgraf@yandex.ru>
2022-05-09 19:37:28 +0300

    Material Design

--- app/src/main/java/ru/netology/NMedia/ViewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/NMedia/ViewModel/PostViewModel.kt
@@ -1,16 +1,43 @@
 package ru.netology.NMedia.ViewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ru.netology.NMedia.data.PostRepository
+import ru.netology.NMedia.Adapter.PostInteractionListener
 import ru.netology.NMedia.data.InMemoryPostRepository
 import ru.netology.NMedia.data.Post
+import ru.netology.NMedia.data.PostRepository
 
-class PostViewModel: ViewModel() {
+class PostViewModel : ViewModel(), PostInteractionListener {
     private val repository: PostRepository = InMemoryPostRepository()
 
     val data by repository::data
 
-    fun onLikeClicked(post: Post) = repository.like(post.id)
+    val currentPost = MutableLiveData<Post?>(null)
 
-    fun onRepostClicked(post: Post) = repository.repost(post.id)
+    fun onSaveClicked(content: String) {
+        if (content.isBlank()) return
+        val post = currentPost.value?.copy(
+            content = content
+        ) ?: Post(
+            id = PostRepository.NEW_POST_ID,
+            author = "Graf Melikov",
+            content = "Hello",
+            date = "06 May 17:36",
+            likes = 0,
+            repost = 0,
+            likedByMe = false
+        )
+        repository.save(post)
+        currentPost.value = null
+    }
+
+    override fun onLikeClicked(post: Post) = repository.like(post.id)
+
+    override fun onRepostClicked(post: Post) = repository.repost(post.id)
+
+    override fun onRemoveClicked(post: Post) = repository.delete(post.id)
+
+    override fun onEditClicked(post: Post) {
+        currentPost.value = post
+    }
 }
